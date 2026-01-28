89fc4ff29f25a6c4aca315a2c6b5c79b66f8beed
Berto <berto@hzlgrn.com>
2026-01-27 20:15:40 -0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,10 @@ buildscript {
 }
 plugins {
     id("com.google.devtools.ksp") version "2.3.4" apply false
+    id("com.google.dagger.hilt.android") version "2.59" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0"
+
 }
 
 
